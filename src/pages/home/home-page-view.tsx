c3d5ec877d8a80be9ef5f935abd2fe52e2fcbcce
widura26 <widurategalrejo@gmail.com>
2024-03-30 16:48:16 +0700

    first commit

---
+++ src/pages/home/home-page-view.tsx
@@ -0,0 +1,7 @@
+const HomePageView = () => {
+    return (
+        <div className="text-2xl font-bold flex justify-center items-center w-full">ini adalah halaman home</div>
+    )
+}
+
+export default HomePageView
